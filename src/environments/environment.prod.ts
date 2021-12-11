2c30423d9630eb7a8dc1217dd6fb1e9cab11e6f2
ale-noga <alexandre2010nogueira@hotmail.com>
2021-12-11 16:20:25 -0300

    install firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'nooexpress',
+    appId: '1:579880661149:web:e2902f49c8046b8d746474',
+    storageBucket: 'nooexpress.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBWkT1XuvGbCKoLKvRrRzYEoSGGibqWqpY',
+    authDomain: 'nooexpress.firebaseapp.com',
+    messagingSenderId: '579880661149',
+    measurementId: 'G-DTPQSS4Q86',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'nooexpress',
+    appId: '1:579880661149:web:e2902f49c8046b8d746474',
+    storageBucket: 'nooexpress.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBWkT1XuvGbCKoLKvRrRzYEoSGGibqWqpY',
+    authDomain: 'nooexpress.firebaseapp.com',
+    messagingSenderId: '579880661149',
+    measurementId: 'G-DTPQSS4Q86',
+  },
   production: false
 };
 
